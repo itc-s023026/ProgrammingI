17f86f1fb1d3169473ed567bfec4ea20d88e2b4c
ryo miyazato <s23026@std.it-college.ac.jp>
2023-07-07 02:07:31 +0000

    リスト

---
+++ CHAPTER02/Q2_7_2.py
@@ -0,0 +1,8 @@
+# 昇順
+my_list = ["orange", "apple", "grape", "banana"]
+print(sorted(my_list))
+
+# 降順
+my_list = ["orange", "apple", "grape", "banana"]
+my_list.sort()
+print(list(reversed(my_list)))
